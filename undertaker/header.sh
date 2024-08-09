3ebbda22a46da37f3c88c9211df0c64b85f1262a
randomscript7 <randomcode_7@outlook.com>
2024-08-09 00:37:55 -0400

    Files integrated
    The undertaker.sh files have finally been sent from my local machine to github. This will kickStart the repository (see what I did there?) and allow me to keep my other machines up to date easier without a USB drive.

---
+++ undertaker/header.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#This is a simple script that puts the undertaker tag above a running script.
+#instead of running two commands, this script serves as a simgle command
+
+clear
+cat logo.txt
+
+exit 0
