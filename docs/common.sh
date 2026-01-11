e19db904b1286d2cd18379bae871eb545be80e49
randomscript7 <sashac11@icloud.com>
2026-01-11 13:29:34 -0500

    Clean up common.sh

--- docs/common.sh
+++ docs/common.sh
@@ -15,22 +15,13 @@ Header () {
 	echo -e "----------------------------------------------------"
 }
 
+# Get the name of the user who invoked the script
+# We do NOT need root's username when the user decides to use sudo
 realUser=$(ls -ld /home/* 2>/dev/null | awk '{print $3}' | head -n 1)
 
-return_real_username() {
-	# Get the name of the user who invoked the script
-	# We do NOT need root's home directory
-	realUser=$(ls -ld /home/* 2>/dev/null | awk '{print $3}' | head -n 1)
-	return "$realUser"
-}
 
 # Function to detect shell and set config file
 setup_shell_config() {
-	# Get the name of the user who invoked the script
-	# We do NOT need root's home directory
-	#realUser=$(ls -ld /home/* 2>/dev/null | awk '{print $3}' | head -n 1)
-	#realUser=$(return_real_username())	
-
 	if [[ -z "$SHELL" ]]; then
 		echo "WARNING: \$SHELL is unset. Assuming bash."
 		SHELL=/bin/bash
